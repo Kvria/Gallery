01adedc8abb2298cf0a5229fa8bf07fb7c764340
Kvria <trishakuria@gmail.com>
2020-05-25 18:51:06 +0300

    add image function

--- gallery/views.py
+++ gallery/views.py
@@ -6,3 +6,10 @@ from django.http  import HttpResponse
 # Create your views here.
 def welcome(request):
     return render(request, 'home.html')
+
+def image(request,image_id):
+    try:
+        image = Image.objects.get(id = image_id)
+    except DoesNotExist:
+        raise Http404()
+    return render(request,"image.html", {"image":image})
