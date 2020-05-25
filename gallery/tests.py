daeaf1ddaaaa1c2298b6da15e2f0ea51d059fc51
Kvria <trishakuria@gmail.com>
2020-05-26 01:19:25 +0300

    add delete method

--- gallery/tests.py
+++ gallery/tests.py
@@ -59,8 +59,14 @@ class LocationTestClass(TestCase):
     def test_instance(self):
         self.assertTrue(isinstance(self.place,Location))
 
-    # Testing Save method
+    # Test Save method
     def test_save_method(self):
         self.place.save_location()
         locations = Location.objects.all()
         self.assertTrue(len(locations)>0)
+
+    # Testing Delete method
+    def test_delete_method(self):
+        self.place.delete_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations)<1)
