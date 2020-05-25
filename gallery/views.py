79e611f3884d1816059c12c47192a8026a282d4b
Kvria <trishakuria@gmail.com>
2020-05-25 19:12:37 +0300

    add search results method

--- gallery/views.py
+++ gallery/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-
-# Create your views here.
-from django.http  import HttpResponse
+from django.http  import HttpResponse,Http404
+from .models import Image
 
 # Create your views here.
 def welcome(request):
@@ -13,3 +12,21 @@ def image(request,image_id):
     except DoesNotExist:
         raise Http404()
     return render(request,"image.html", {"image":image})
+
+def search_results(request):
+ 
+    if 'image'in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        searched_image = Image.search_by_location_id(search_term)
+        message = f"{search_term}"
+
+    elif 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        searched_image = Image.search_by_category_id(search_term)
+        message = f"{search_term}"
+
+        return render(request, '',{"message":message,"images": searched_images})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, '',{"message":message})
