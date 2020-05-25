21dc2d394d13f03d7f5277d59590f49c811b7bfc
Kvria <trishakuria@gmail.com>
2020-05-26 01:17:51 +0300

    add saving method

--- gallery/tests.py
+++ gallery/tests.py
@@ -58,3 +58,9 @@ class LocationTestClass(TestCase):
     # Test instance
     def test_instance(self):
         self.assertTrue(isinstance(self.place,Location))
+
+    # Testing Save method
+    def test_save_method(self):
+        self.place.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations)>0)
