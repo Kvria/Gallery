dbe1ff10379e5ab3e5d2a0c071ca6dba74c4c910
Kvria <trishakuria@gmail.com>
2020-05-28 01:35:07 +0300

    add whitenoise features

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn pixels-gallery.wsgi --log-file -
--- pixels/settings.py
+++ pixels/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -132,3 +133,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
+# Configure Django App for Heroku.
+django_heroku.settings(locals())
